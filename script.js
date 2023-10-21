51c5ed67d3e9b8abffe763c17c2b604250f760d7
quiro <tinyquiroga@gmail.com>
2023-10-21 20:13:23 -0300

    cambios galerias

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.getElementById('next').onclick = function(){
+///GALERIA
+document.getElementById('next').onclick = function () {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').appendChild(lists[0]);
 }
@@ -6,29 +7,25 @@ document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
+///CURSOR
 document.addEventListener("DOMContentLoaded", function () {
     const customCursor = document.querySelector(".custom-cursor");
 
     document.addEventListener("mousemove", (e) => {
-        // Obtén las coordenadas relativas al documento
         const x = e.clientX;
         const y = e.clientY;
 
     customCursor.style.left = (x + 50) + "px";
     customCursor.style.top = (y + 30) + "px";
     });
-
-    // Muestra el cursor personalizado cuando se mueve el ratón
     document.addEventListener("mouseenter", () => {
         customCursor.style.display = "block";
     });
-
-    // Oculta el cursor personalizado cuando sale del área del cursor
     document.addEventListener("mouseleave", () => {
         customCursor.style.display = "none";
     });
 });
-
+///ANIMACION NAV
 document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("scroll", function() {
     var botonera = document.querySelector('.botonera');
