7ea4f31accee4f7d29e17bac3660ded1dc1693de
quiro <tinyquiroga@gmail.com>
2023-10-21 16:39:11 -0300

    cambios estilo

--- script.js
+++ script.js
@@ -28,3 +28,14 @@ document.addEventListener("DOMContentLoaded", function () {
         customCursor.style.display = "none";
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener("scroll", function() {
+    var botonera = document.querySelector('.botonera');
+    if (window.scrollY > 100) {
+      botonera.classList.add('scrolled');
+    } else {
+      botonera.classList.remove('scrolled');
+    }
+  });
+});
